66263264dc5516d892afea1e5fc963f2b8f194a2
Nic0M <89534840+Nic0M@users.noreply.github.com>
2024-03-14 04:53:31 -0600

    New search with edit distance bugfix

--- wp-content/themes/astra-child-theme/assets/js/unminified/database_tables.js
+++ wp-content/themes/astra-child-theme/assets/js/unminified/database_tables.js
@@ -145,7 +145,7 @@ function createSearchBar(search_bar_div, table_id) {
 	search_bar.setAttribute('class', 'form-control'); // Bootstrap (CSS framework) class
 	// Add event listener to search bar
 	search_bar.addEventListener('input', function() {
-		search_table(search_bar_id, table_id, 0, false); // Only search first column and make search case-insensitive
+		search_table(search_bar_id, table_id, [0], false); // Only search first column and make search case-insensitive
 		// searchTable(search_bar_id, table_id);
 	});
 
