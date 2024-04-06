837b781c6d4a9c943d398cc1090e77f8a6c897a4
Nic0M <89534840+Nic0M@users.noreply.github.com>
2024-04-05 23:54:08 -0600

    Updated cronjob

--- wp-content/themes/astra-child-theme/cronjob.php
+++ wp-content/themes/astra-child-theme/cronjob.php
@@ -4,7 +4,7 @@ require_once('/opt/bitnami/wordpress/wp-load.php');
 	
 function update_healthcheck_status_callback() {
 	global $dronedb;
-	$result = $dronedb->query("UPDATE `healthchecks` SET `Status` = 'Unhealthy' WHERE `updated_at` <= NOW() - INTERVAL 5 MINUTE" );
+	$result = $dronedb->query("UPDATE healthchecks SET Status = 'Unhealthy', updated_at = NOW() WHERE updated_at <= NOW() - INTERVAL 5 MINUTE" );
 	if ($dronedb->last_error) {
 		error_log($dronedb->last_error);
 	}
