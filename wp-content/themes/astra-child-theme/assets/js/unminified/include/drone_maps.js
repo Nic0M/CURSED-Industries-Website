5b44624ef6b12c521642a94e7e2ec70303dbc116
Nic0M <89534840+Nic0M@users.noreply.github.com>
2024-04-08 01:52:15 -0600

    Testing opacity and wider angles

--- wp-content/themes/astra-child-theme/assets/js/unminified/include/drone_maps.js
+++ wp-content/themes/astra-child-theme/assets/js/unminified/include/drone_maps.js
@@ -33,6 +33,7 @@ var DroneIconClass = L.Icon.extend(
 			iconSize: [100, 100],
 			iconAnchor: [50, 50], // so middle of drone is center
 			popupAnchor: [0, -50], // so popup is above drone
+			rotationOrigin: 'center center',
 		}
 	},
 );
@@ -45,7 +46,7 @@ var cyan_icon = new DroneIconClass({iconUrl: 'https://cursedindustries.com/image
 var lilac_icon = new DroneIconClass({iconUrl: 'https://cursedindustries.com/images/drone_marker_icon_lilac.png'});
 var pink_icon = new DroneIconClass({iconUrl: 'https://cursedindustries.com/images/drone_marker_icon_pink.png'});
 
-red_marker = L.marker([40.03, -105.1], {icon: red_icon}).addTo(live_map).bindPopup("I'm a red drone!");
+red_marker = L.marker([40.03, -105.1], {icon: red_icon}).addTo(live_map).bindPopup("I'm a red drone moving at 50 m/s!");
 orange_marker = L.marker([40.04, -105.1], {icon: orange_icon}).addTo(live_map).bindPopup("I'm an orange drone!");
 yellow_marker = L.marker([40.05, -105.1], {icon: yellow_icon}).addTo(live_map).bindPopup("I'm a yellow drone!");
 green_marker = L.marker([40.06, -105.1], {icon: green_icon}).addTo(live_map).bindPopup("I'm a green drone!");
@@ -64,11 +65,17 @@ purple_marker.setRotationAngle(315);
 // Create made up path for red drone
 var red_drone_path_lat_lon = [
 	[40.022, -103.2],
-	[40.023, -103.3],
-	[40.027, -103.4],
-	[40.031, -103.5],
+	[40.023, -106.3],
+	[40.027, -102.4],
+	[40.031, -104.5],
 	[40.035, -103.87],
 	[40.03, -105.1],
 ];
 
-var red_polyline = L.polyline(red_drone_path_lat_lon, {color: '#ff3131'}).addTo(live_map);
+var red_polyline = L.polyline(
+	red_drone_path_lat_lon,
+	{
+		color: '#ff3131',
+		opacity: 0.8,
+	},
+).addTo(live_map);
