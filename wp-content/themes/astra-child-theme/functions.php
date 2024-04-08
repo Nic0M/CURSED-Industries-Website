af9cdfe47d1f36e4a377422eb99a4cdfdea52de0
Nic0M <89534840+Nic0M@users.noreply.github.com>
2024-04-08 14:10:17 -0600

    Show error

--- wp-content/themes/astra-child-theme/functions.php
+++ wp-content/themes/astra-child-theme/functions.php
@@ -262,20 +262,26 @@ function get_flight_packets($data) {
 	// Select the flight from the database
 	$completed_flights_table_name = 'completed_flights';
 	$remoteid_packets_table_name = 'remoteid_packets';
-	$query = $dronedb->prepare('Select rp.unique_id,rp.timestamp,rp.heading,rp.gnd_speed,rp.vert_speed,rp.lat,rp.lon,rp.height
+	$query = $dronedb->prepare(
+		'Select rp.unique_id,rp.timestamp,rp.heading,rp.gnd_speed,rp.vert_speed,rp.lat,rp.lon,rp.height
 					From %s cf, %s rp
 					where rp.src_addr=cf.src_addr and rp.src_addr="%s" and rp.timestamp >= "%s" 
 						and rp.timestamp <= 
 							(Select end_time
 								From completed_flights
         							Where src_addr="%s" and start_time="%s");',
-				   					$completed_flights_table_name, $remoteid_packets_table_name,
-				   					$src_addr,$start_time,$src_addr,$start_time);
+		$completed_flights_table_name,
+		$remoteid_packets_table_name,
+		$src_addr,
+		$start_time,$src_addr,
+		$start_time
+	);
 	$results = $dronedb->get_results($query);
 
 	// Check for error
 	if ($dronedb->last_error) {
 		error_log($dronedb->last_error);
+		return new WP_REST_Response(array('error' => $dronedb->last_error));
 		return new WP_REST_Response(array('error' => 'Internal Server Error'), 500);
   	}
 
