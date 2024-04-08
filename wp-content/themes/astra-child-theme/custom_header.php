c9d1646d9d73c58997a6198a27a8be3bdef44360
Nic0M <89534840+Nic0M@users.noreply.github.com>
2024-04-07 23:18:21 -0600

    Fixed leaflet css include in header

--- wp-content/themes/astra-child-theme/custom_header.php
+++ wp-content/themes/astra-child-theme/custom_header.php
@@ -15,7 +15,7 @@ add_action('wp_head', 'add_leaflet');
 function add_leaflet() {
 	// Add Leaflet CSS
 	?>
-	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css
+	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
 		integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
 		crossorigin=""/>
 	<?php
